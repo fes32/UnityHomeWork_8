55e73352c0161f450ac7a74f209ea7d089e1d8f7
fes <fesss32@yandex.ru>
2020-12-21 16:00:41 +0500

    Init

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    private Animator _animator;
+
+    public void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void StartAnimation()
+    {
+        _animator.SetTrigger("Start");
+    }
+}
